6ece932e1d615f85527330d60fe4b992815a136b
Hannes Löwenberg <hannes@zonidor.com>
2020-04-04 00:12:11 +0200

    name input check for proper input

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function closePopUp () {
-    document.querySelector('#popup').remove()
-    document.querySelector('#window-wrapper').style.filter = 'none'
+    var nameInput = document.querySelector('#name-input')
+    if (nameInput.value && nameInput.value.length > 5) {
+        document.querySelector('#popup').remove()
+        document.querySelector('#window-wrapper').style.filter = 'none'
+    }
+    else if (nameInput.value && nameInput.value.length <= 5) {
+        nameInput.value = 'Name too short'
+    }
+    else {
+        nameInput.value = 'Did you forget something?'
+    }
 }
