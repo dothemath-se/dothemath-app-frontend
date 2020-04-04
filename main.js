5c65180599175df455a6ac4bc7b6476ae78786a5
Hannes Löwenberg <zonidor@gmail.com>
2020-04-04 12:08:06 +0200

    add first steps for socket.io

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function closePopUp () {
     var nameInput = document.querySelector('#name-input')
-    if (nameInput.value && nameInput.value.length > 5) {
+    if (nameInput.value && nameInput.value.length > 1) {
         document.querySelector('#popup').remove()
         document.querySelector('#window-wrapper').style.filter = 'none'
     }
-    else if (nameInput.value && nameInput.value.length <= 5) {
+    else if (nameInput.value && nameInput.value.length <= 1) {
         nameInput.value = 'Name too short'
     }
     else {
