314292c7475fb187ad828fd5fc719d076e426d72
Hannes Löwenberg <zonidor@gmail.com>
2020-04-04 12:20:43 +0200

    socket.io local implementation

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
