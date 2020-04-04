f28ef0997926d124e37dd3fedf5fd59fdc9c87d4
Hannes Löwenberg <zonidor@gmail.com>
2020-04-04 13:31:29 +0200

    add first implementation of name

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+function setName () {
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)name\s*\=\s*([^;]*).*$)|^.*$/, "$1") == '') {
+        document.querySelector('#window-wrapper').style.filter = 'blur(5px)'
+
+        var namePopup = document.createElement('div')
+
+        namePopup.setAttribute('id', 'popup')
+        namePopup.innerHTML= '<div id="popup-container"><h3>Hey there buddy! <br> Who are you?</h3><input id="name-input" type="text" placeholder="Hi, my name is..."><button class="btn--primary" onclick="closePopUp()">ENTER</button></div>'
+
+        document.body.appendChild(namePopup)
+    }
+}
+
 function closePopUp () {
     var nameInput = document.querySelector('#name-input')
     if (nameInput.value && nameInput.value.length > 1) {
+        document.cookie ='name=' + nameInput.value
         document.querySelector('#popup').remove()
         document.querySelector('#window-wrapper').style.filter = 'none'
     }
