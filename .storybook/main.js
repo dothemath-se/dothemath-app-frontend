0ab999d66c85f6cd631cc9e734ec942d80ded0c0
Magnus Markling <magnus@markling.com>
2020-05-13 11:26:17 +0200

    alter storybook config to disable webpack warning

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,12 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+  webpackFinal: async (config, { configType }) => {
+    // disable webpack warnings about bundle size
+    // https://github.com/storybookjs/storybook/pull/6390#issuecomment-588149281
+    // https://storybook.js.org/docs/configurations/custom-webpack-config/
+    config.performance.hints = false;
+
+    return config;
+  },
 };
