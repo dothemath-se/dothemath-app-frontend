9cc72d5c083f6aedfba29ce64ec00d0dca659f54
Magnus Markling <magnus@markling.com>
2020-05-13 15:36:51 +0200

    improve Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,14 +5,23 @@ export default {
   title: 'Button',
 };
 
-export const Default = () => (
+export const Normal = () => (
   <div>
-    <Button onClick={() => console.log('Button clicked')}>Default</Button>
-    <Button primary onClick={() => console.log('Button clicked')}>
-      Primary
+    <Button onClick={logClick}>default</Button>
+    <br />
+    <br />
+    <Button disabled onClick={logClick}>
+      default disabled
     </Button>
-    <Button disabled primary>
-      Disabled
+    <br />
+    <br />
+    <Button primary onClick={logClick}>
+      primary
+    </Button>
+    <br />
+    <br />
+    <Button primary disabled onClick={logClick}>
+      primary disabled
     </Button>
   </div>
 );
@@ -20,9 +29,11 @@ export const Default = () => (
 export const Link = () => (
   <span>
     This is an{' '}
-    <Button link onClick={() => console.log('Button clicked')}>
+    <Button link onClick={logClick}>
       inline link
     </Button>{' '}
     button.
   </span>
 );
+
+const logClick = (e) => console.log(e.target);
