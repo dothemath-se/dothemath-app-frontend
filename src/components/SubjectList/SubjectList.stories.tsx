7f48f70399bea10836b2671be59121f6286332a9
Magnus Markling <magnus@markling.com>
2020-05-22 15:02:48 +0200

    move api call to SubjectList

--- src/components/SubjectList/SubjectList.stories.tsx
+++ src/components/SubjectList/SubjectList.stories.tsx
@@ -5,20 +5,4 @@ export default {
   title: 'SubjectList',
 };
 
-export const Math1 = () => (
-  <SubjectList
-    data={[{ name: 'Math 1', id: 'ChannelId123' }]}
-    onComplete={() => {}}
-  />
-);
-
-export const ManySubjects = () => (
-  <SubjectList
-    data={Array.from({ length: 10 }, (_, i) => ({
-      name: `Math ${i + 1}`,
-      id: i.toString(),
-      key: i,
-    }))}
-    onComplete={() => {}}
-  />
-);
+export const Default = () => <SubjectList onComplete={() => {}} />;
