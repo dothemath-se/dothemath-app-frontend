7cfa4dfc1a1123abe0ac2cd537607df515e52c22
Magnus Markling <magnus@markling.com>
2020-05-24 13:00:54 +0200

    add types to useCookie()

--- src/useCookie.ts
+++ src/useCookie.ts
@@ -1,12 +1,12 @@
 import { useCookies } from 'react-cookie';
 import { useDebugValue } from 'react';
 
-export const useCookie = (cookieName: string) => {
+export const useCookie = <T>(cookieName: string): [T, (value: T) => void] => {
   const [cookies, setCookie] = useCookies([cookieName]);
   useDebugValue(`${cookieName}: ${cookies[cookieName]}`);
   return [
     cookies[cookieName],
-    (value: string) =>
+    (value: T) =>
       setCookie(cookieName, value, { path: '/', maxAge: 30 * 24 * 60 * 60 }),
   ];
 };
