7639d28efd28f627cb7d6b4ab5eafa87f5be6ecf
Magnus Markling <magnus@markling.com>
2020-06-01 13:28:48 +0200

    move window-wrapper and logos from Chat to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -17,45 +17,65 @@ export const App = () => {
 
   return (
     <>
-      <Switch>
-        <Route exact path="/">
-          {!name ? (
-            <Redirect to="/start" />
-          ) : !subject ? (
-            <Redirect to="/subject" />
-          ) : (
-            <Redirect to="/chat" />
-          )}
-        </Route>
-        <Route path="/start">
-          <Popup
-            onComplete={(newName) => {
-              setName(newName);
-              history.replace('/subject');
-            }}
+      <div id="window-wrapper">
+        <Switch>
+          <Route exact path="/">
+            {!name ? (
+              <Redirect to="/start" />
+            ) : !subject ? (
+              <Redirect to="/subject" />
+            ) : (
+              <Redirect to="/chat" />
+            )}
+          </Route>
+          <Route path="/start">
+            <Popup
+              onComplete={(newName) => {
+                setName(newName);
+                history.replace('/subject');
+              }}
+            />
+          </Route>
+          <Route path="/subject">
+            <SubjectList
+              onComplete={(newSubject) => {
+                setSubject(newSubject);
+                setThreadId('');
+                history.replace('/chat');
+              }}
+            />
+          </Route>
+          <Route path="/chat">
+            <Chat
+              name={name}
+              subject={subject}
+              threadId={threadId}
+              setThreadId={setThreadId}
+              onNewQuestionClick={() => {
+                history.replace('/subject');
+              }}
+            />
+          </Route>
+        </Switch>
+        <img
+          id="logo"
+          src="img/logo_white.svg"
+          alt="Site Logo"
+          draggable="false"
+        />
+        <a
+          href="https://vercel.com/?utm_source=dothemath"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            id="logo-vercel"
+            src="img/powered-by-vercel.svg"
+            alt="Powered by Vercel"
+            draggable="false"
           />
-        </Route>
-        <Route path="/subject">
-          <SubjectList
-            onComplete={(newSubject) => {
-              setSubject(newSubject);
-              setThreadId('');
-              history.replace('/chat');
-            }}
-          />
-        </Route>
-        <Route path="/chat">
-          <Chat
-            name={name}
-            subject={subject}
-            threadId={threadId}
-            setThreadId={setThreadId}
-            onNewQuestionClick={() => {
-              history.replace('/subject');
-            }}
-          />
-        </Route>
-      </Switch>
+        </a>
+      </div>
     </>
   );
 };
