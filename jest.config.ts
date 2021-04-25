178657a6d835656417450d9868e1611652abe6ce
Magnus Markling <magnus@markling.com>
2021-04-25 17:11:34 +0200

    clean jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,9 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  // testEnvironment: 'node',
-  // testEnvironment: 'jest-environment-node',
-  transform: {},
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
-    // '\\.(css|less|sass)$': '<rootDir>/src/styleMock.ts',
-    '\\.(css|less|sass)$': 'identity-obj-proxy',
+    '\\.(css|sass)$': 'identity-obj-proxy',
   },
-  // moduleDirectories: ['node_modules', '<rootDir>/node_modules', '.'],
 };
 export default config;
