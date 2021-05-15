e81ad7e101eb64fcbda98b65b4c8d81df7078902
Magnus Markling <magnus@markling.com>
2021-05-15 14:55:01 +0200

    set up cypress

---
+++ cypress/dtm.spec.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+/* eslint-disable jest/expect-expect */
+
+describe('dothemath.app', () => {
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('name', 'subject', 'threadId');
+  });
+
+  it('Registration', () => {
+    cy.visit('/');
+    cy.contains('Innan vi börjar...');
+    cy.contains('button', 'Börja').should('be.disabled');
+    cy.get('[placeholder^="Välj ett"]').type('Cypress');
+    cy.contains('förstår och accepterar').click();
+    cy.contains('har läst och accepterar').click();
+    cy.contains('button', 'Börja').should('be.enabled').click();
+    cy.contains('Välj ämne');
+    cy.contains('Bot-Test', { matchCase: false });
+  });
+
+  it('Subject List', () => {
+    cy.visit('/');
+    cy.contains('Välj ämne');
+    cy.contains('Bot-Test', { matchCase: false }).click();
+    cy.contains('Ställ en ny');
+  });
+
+  it('Chat', () => {
+    cy.visit('/');
+    cy.contains('Ställ en ny');
+    cy.contains('Bot-Test', { matchCase: false });
+    cy.get('[placeholder^="Skriv"]').type('testmeddelande 1{enter}');
+    cy.contains('p', 'testmeddelande 1');
+    cy.get('[placeholder^="Skriv"]').type('testmeddelande 2');
+    cy.get('button[aria-label^="Skicka"]').click();
+    cy.contains('p', 'testmeddelande 1');
+    cy.contains('p', 'testmeddelande 2');
+  });
+
+  it('Chat revisited', () => {
+    cy.visit('/');
+    cy.contains('Ställ en ny');
+    cy.contains('Bot-Test', { matchCase: false });
+    cy.contains('p', 'testmeddelande 1');
+    cy.contains('p', 'testmeddelande 2');
+  });
+});
