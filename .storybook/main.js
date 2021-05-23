9aa5283338a43977e65c08d258e9ad866882821a
Magnus Markling <magnus@markling.com>
2021-05-23 10:26:06 +0200

    set build.emptyOutDir = true

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "core": {
-    "builder": "storybook-builder-vite"
-  }
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+  async viteFinal(config, { configType }) {
+    config.build.emptyOutDir = true;
+
+    return config;
+  },
+};
