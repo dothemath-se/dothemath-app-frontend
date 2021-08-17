cd0ab2448bb9b318f4e7d918350a66029f49fac3
Magnus Markling <magnus@markling.com>
2021-08-17 09:32:58 +0200

    fix lint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -15,64 +15,64 @@ export const App = () => {
   const history = useHistory();
 
   return (
-      <div className="app--window-wrapper">
-        <Switch>
-          <Route exact path="/">
-            {!name ? (
-              <Redirect to="/registration" />
-            ) : !subject ? (
-              <Redirect to="/subject" />
-            ) : (
-              <Redirect to="/chat" />
-            )}
-          </Route>
-          <Route path="/registration">
-            <Registration
-              onComplete={(newName) => {
-                setName(newName);
-                history.replace('/subject');
-              }}
-            />
-          </Route>
-          <Route path="/subject">
-            <SubjectList
-              onComplete={(newSubject) => {
-                setSubject(newSubject);
-                setThreadId('');
-                history.replace('/chat');
-              }}
-            />
-          </Route>
-          <Route path="/chat">
-            <Chat
-              name={name}
-              subject={subject}
-              threadId={threadId}
-              setThreadId={setThreadId}
-              onNewQuestionClick={() => {
-                history.replace('/subject');
-              }}
-            />
-          </Route>
-        </Switch>
-        <img
-          src="img/logo_white.svg"
-          alt="Site Logo"
-          draggable="false"
-          className="app--logo"
-        />
-        <a
-          href="https://vercel.com/?utm_source=dothemath"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="app--logo-vercel"
-        >
-          <img
-            src="img/powered-by-vercel.svg"
-            alt="Powered by Vercel"
-            draggable="false"
+    <div className="app--window-wrapper">
+      <Switch>
+        <Route exact path="/">
+          {!name ? (
+            <Redirect to="/registration" />
+          ) : !subject ? (
+            <Redirect to="/subject" />
+          ) : (
+            <Redirect to="/chat" />
+          )}
+        </Route>
+        <Route path="/registration">
+          <Registration
+            onComplete={(newName) => {
+              setName(newName);
+              history.replace('/subject');
+            }}
           />
-        </a>
-      </div>
+        </Route>
+        <Route path="/subject">
+          <SubjectList
+            onComplete={(newSubject) => {
+              setSubject(newSubject);
+              setThreadId('');
+              history.replace('/chat');
+            }}
+          />
+        </Route>
+        <Route path="/chat">
+          <Chat
+            name={name}
+            subject={subject}
+            threadId={threadId}
+            setThreadId={setThreadId}
+            onNewQuestionClick={() => {
+              history.replace('/subject');
+            }}
+          />
+        </Route>
+      </Switch>
+      <img
+        src="img/logo_white.svg"
+        alt="Site Logo"
+        draggable="false"
+        className="app--logo"
+      />
+      <a
+        href="https://vercel.com/?utm_source=dothemath"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="app--logo-vercel"
+      >
+        <img
+          src="img/powered-by-vercel.svg"
+          alt="Powered by Vercel"
+          draggable="false"
+        />
+      </a>
+    </div>
   );
 };
